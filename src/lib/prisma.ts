007d4d40d1f83621c12a6d3521d06c0bf337af88
jambo <jambo@kali.kali>
2026-03-19 22:59:31 +0200

    fix: revert to native prisma rust engine to fix vercel build-time env variable omission

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL;
-  const pool = new Pool({ connectionString })
-  // @ts-ignore
-  const adapter = new PrismaNeon(pool)
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 declare const globalThis: {
