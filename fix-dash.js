117cfceaead74fab68f4c51cccd5d486725a1409
jambo <jambo@kali.kali>
2026-03-20 07:25:15 +0200

    chore: clean up root directory debugging scripts

--- fix-dash.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let pDash = 'src/app/manager/dashboard/page.tsx';
-let cDash = fs.readFileSync(pDash, 'utf8');
-cDash = cDash.replace(/        <\/div>\n      <\/main>\n  <\/div>\n  \);\n}/, '      </main>\n  </div>\n  );\n}'); 
-fs.writeFileSync(pDash, cDash);
-
